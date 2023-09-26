65d5a6ef1b9f3ca30b4e7da8ae57c36f8c50d32e
armanpinachyan <pinachyan99@mail.ru>
2023-09-26 18:03:36 +0400

    18:03

--- src/pages/profile-settings/profile-settings.tsx
+++ src/pages/profile-settings/profile-settings.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './profile-settings.css';
 import DefaultInputs from '../../features/defultinputs/Defultinputs';
+import MainTemplate from '../../features/main-template/main-template';
 
 function ProfileSettings() {
   return (
-    <div className="CreateProject">
-      <div className="container"></div>
-    </div>
+    <MainTemplate className="reviewModeration">
+      <div className="CreateProject">
+        <div className="container">
+          <div className="SetpUi">
+            <div className="inner">
+              <div className="box active"></div>
+              <div className="border"></div>
+              <div className="box"></div>
+              <div className="border"></div>
+              <div className="box"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </MainTemplate>
   );
 }
 
