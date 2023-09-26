b89b5e4151a44ac8e54e205bbf06aead191f7b8f
armanpinachyan <pinachyan99@mail.ru>
2023-09-26 13:53:27 +0400

    13:53

--- src/features/navbar/navbar.tsx
+++ src/features/navbar/navbar.tsx
@@ -4,6 +4,9 @@ import Logo from '../../assets/images/logo-site.svg';
 import rubleIcon from '../../assets/images/ruble-icon.svg';
 import { Link } from 'react-router-dom';
 import { Badge, Button, IconButton } from '@mui/material';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropDownWb from '../../assets/images/DropDownWb.svg';
+import DropDownOz from '../../assets/images/DropDownOz.svg';
 
 function Navbar() {
   return (
@@ -20,6 +23,37 @@ function Navbar() {
           {/*  Настройка отзывов*/}
           {/*  <i className="fa-regular fa-chevron-down ms-2" />*/}
           {/*</Link>*/}
+          <Dropdown className="DropDown">
+            <Dropdown.Toggle className="DropDownBtn" variant="success" id="dropdown-basic">
+              <img src={DropDownWb} alt="img" />
+              WB ИП ФИО...(№ 1111)
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu className="DropDownItem">
+              <Dropdown.Item className="DropDownItems" href="#/action-1">
+                <img src={DropDownOz} alt="img" />
+                ozon.ru ООО “Рога и копаты инкорп...
+              </Dropdown.Item>
+              <Dropdown.Item className="DropDownItems" href="#/action-2">
+                <img src={DropDownWb} alt="img" />
+                Wildberries ООО Название
+              </Dropdown.Item>
+              <Dropdown.Item className="DropDownItems" href="#/action-3">
+                <img src={DropDownOz} alt="img" />
+                ozon ИП Название
+              </Dropdown.Item>
+              <Dropdown.Item className="DropDownItems" href="#/action-3">
+                <img src={DropDownWb} alt="img" />
+                Wildberries ИП Название
+              </Dropdown.Item>
+              <Link to="#" className="addShop">
+                <div className="IconPlus">
+                  <i className="fa-solid fa-plus"></i>
+                </div>
+                добавить МАГАЗИН
+              </Link>
+            </Dropdown.Menu>
+          </Dropdown>
           <Link to="#" className="nav-item">
             Статистика
           </Link>
