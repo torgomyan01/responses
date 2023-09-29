22406c06c0f7ac7589fdb83ad3d28bf93745a873
armanpinachyan <pinachyan99@mail.ru>
2023-09-29 18:32:13 +0400

    55

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,6 +5,9 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export const GetUserStores = () => axios.get(`${API_URL}${API_URLS.STORES}`);
 
+export const DeleteUserStores = (id: number | undefined) =>
+  axios.delete(`${API_URL}${API_URLS.STORES}/${id}`);
+
 export const GetStoreInfo = (id: string) => axios.get(`${API_URL}${API_URLS.GET_STORE(id)}`);
 
 export const CreateStore = (data: IStores) => axios.post(`${API_URL}${API_URLS.STORES}`, data);
