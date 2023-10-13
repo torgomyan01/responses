bf351c70c4ebf94ed88549addae12cb237a094c3
Andranik Torgomyan <andranik@nteams.com>
2023-10-13 14:43:55 +0400

    end task Меню 2 уровня

--- src/features/shape/shape.tsx
+++ src/features/shape/shape.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './shape.scss';
 import updateButton from '../../assets/images/update-button.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { SITE_URL } from '../../utils/const';
 
 function Shape() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
+
   function ReloadPage() {
     window.location.reload();
   }
   return (
     <div className="shape">
       <div>
-        <Link className="shape-item" to={`../${SITE_URL.PRODUCTS}`}>
+        <Link
+          className={`shape-item ${location.pathname.includes(SITE_URL.PRODUCTS) ? 'green' : ''}`}
+          to={`../${SITE_URL.PRODUCTS}`}>
           <i className="fa-regular fa-gift me-2" />
           Список товаров
         </Link>
       </div>
       <div>
-        <Link className="shape-item green" to={`../${SITE_URL.FEEDBACKS}`}>
+        <Link
+          className={`shape-item ${location.pathname.includes(SITE_URL.FEEDBACKS) ? 'green' : ''}`}
+          to={`../${SITE_URL.FEEDBACKS}`}>
           <i className="fa-regular fa-message-lines fa-flip-horizontal me-2" />
           Отзывы
         </Link>
